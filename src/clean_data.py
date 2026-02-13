ca9d5f1add3c249126f29c4a415097b4fdc49478
rakeshroyal4a0 <rakeshroyal344@gmail.com>
2026-02-13 13:33:54 +0530

    Fix clean_data path

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,19 +1,15 @@
 import os
 from PIL import Image
 
-# Find correct PetImages folder automatically
-base_path = "data/raw"
-
-# search for PetImages folder dynamically
-for root, dirs, files in os.walk(base_path):
-    if "Cat" in dirs and "Dog" in dirs:
-        data_dir = root
-        break
-
-print(f"Using dataset path: {data_dir}")
+# Correct path after unzip in GitHub Actions
+data_dir = "data/raw/PetImages"
 
 for category in ["Cat", "Dog"]:
     folder_path = os.path.join(data_dir, category)
+
+    if not os.path.exists(folder_path):
+        print(f"Folder not found: {folder_path}")
+        continue
 
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
