fd7571f131770c2cff21124aeecc8248e5ac9a51
rakeshroyal4a0 <rakeshroyal344@gmail.com>
2026-02-13 13:25:50 +0530

    Fix dynamic dataset path

--- src/clean_data.py
+++ src/clean_data.py
@@ -1,14 +1,23 @@
 import os
 from PIL import Image
 
-data_dir = "data/raw/PetImages"
+# Find correct PetImages folder automatically
+base_path = "data/raw"
+
+# search for PetImages folder dynamically
+for root, dirs, files in os.walk(base_path):
+    if "Cat" in dirs and "Dog" in dirs:
+        data_dir = root
+        break
+
+print(f"Using dataset path: {data_dir}")
 
 for category in ["Cat", "Dog"]:
     folder_path = os.path.join(data_dir, category)
-    
+
     for file in os.listdir(folder_path):
         file_path = os.path.join(folder_path, file)
-        
+
         try:
             img = Image.open(file_path)
             img.verify()
